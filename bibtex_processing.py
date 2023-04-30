59faa430cc8b0bf745059ab09c175502c343ff29
Tina Lasisi <tina.lasisi@gmail.com>
2023-04-30 13:06:29 -0700

    Fixed issues with missing bibtex content
    Fixed issues with missing bibtex content (e.g. when journal isn't present). Still need to clean all the functions that have been commented out.

--- bibtex_processing.py
+++ bibtex_processing.py
@@ -4,8 +4,32 @@ import os
 import glob
 import bibtexparser
 
+# def generate_pattern(entry, mode=1):
+#     journal = entry.get('journal', '')  # Use an empty string if 'journal' doesn't exist
+#     year = entry['year']
+#     title = entry['title'].replace(' ', '-').replace('{', '').replace('}', '').replace('\n', '')
+
+#     authors = entry['author'].split(' and ')
+#     for i in range(len(authors)):
+#         if ',' in authors[i]:  # if author is in format "LastName, FirstName"
+#             authors[i] = authors[i].split(',')[0]  # take only the last name
+
+#     if len(authors) > 2:
+#         authors = authors[0] + "-et-al"
+#     elif len(authors) == 2:
+#         authors = '-and-'.join(authors)
+#     else:  # There is only one author
+#         authors = authors[0]
+
+#     if mode == 1:
+#         return f"{authors}_{year}_{title}"
+#     else:  # mode 2
+#         return f"{authors}_{journal}_{year}_{title}"
+
+import string
+
 def generate_pattern(entry, mode=1):
-    journal = entry['journal']
+    journal = entry.get('journal', '')  # Use an empty string if 'journal' doesn't exist
     year = entry['year']
     title = entry['title'].replace(' ', '-').replace('{', '').replace('}', '').replace('\n', '')
 
@@ -21,10 +45,23 @@ def generate_pattern(entry, mode=1):
     else:  # There is only one author
         authors = authors[0]
 
+    valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+
     if mode == 1:
-        return f"{authors}_{year}_{title}"
+        pattern = f"{authors}_{year}_{title}"
     else:  # mode 2
-        return f"{authors}_{journal}_{year}_{title}"
+        pattern = f"{authors}_{journal}_{year}_{title}"
+
+    # Remove invalid characters
+    pattern = ''.join(c for c in pattern if c in valid_chars)
+
+    # Limit the length of the pattern to prevent OSError: [Errno 63] File name too long
+    max_length = 250  # Maximum filename length for most filesystems
+    if len(pattern) > max_length:
+        pattern = pattern[:max_length]
+
+    return pattern
+
 
 
 
--- main.py
+++ main.py
@@ -54,6 +54,25 @@ if not bib_files:
 
 print("Found .bib file.")
 
+# # Generate a list of filenames from the .bib file and save the entries
+# print("Generating filenames from .bib file...")
+# filenames, entries = process_bibtex(bib_files[0])
+# entries_dict = {filename + '.pdf': entry for filename, entry in zip(filenames, entries)}
+# print("Done generating filenames.")
+
+# # Initialize the Google Drive service
+# print("Initializing Google Drive service...")
+# drive_service = load_gdrive_creds()
+# print("Done initializing Google Drive service.")
+
+# # Download PDFs for each filename and save them in the 'pdf' folder
+# print("Downloading PDFs...")
+# for filename in filenames:
+#     file_id = find_closest_match(drive_service, filename)
+#     if file_id:
+#         download_pdf(drive_service, file_id, os.path.join(pdf_folder_path, filename + '.pdf'))
+# print("Done downloading PDFs.")
+
 # Generate a list of filenames from the .bib file and save the entries
 print("Generating filenames from .bib file...")
 filenames, entries = process_bibtex(bib_files[0])
@@ -68,18 +87,66 @@ print("Done initializing Google Drive service.")
 # Download PDFs for each filename and save them in the 'pdf' folder
 print("Downloading PDFs...")
 for filename in filenames:
-    file_id = find_closest_match(drive_service, filename)
-    if file_id:
-        download_pdf(drive_service, file_id, os.path.join(pdf_folder_path, filename + '.pdf'))
+    full_file_path = os.path.join(pdf_folder_path, filename + '.pdf')
+    if not os.path.isfile(full_file_path):  # Check if file already exists
+        file_id = find_closest_match(drive_service, filename)
+        if file_id:
+            download_pdf(drive_service, file_id, full_file_path)
+        else:
+            print(f"No match found for {filename}. Skipping this file.")
+    else:
+        print(f"File {filename} already exists. Skipping download.")
 print("Done downloading PDFs.")
+
+
+# # Convert each PDF to a txt file and save it in the 'txt' folder
+# print("Converting PDFs to txt files...")
+# pdf_files = glob.glob(os.path.join(pdf_folder_path, "*.pdf"))
+# for pdf_file in pdf_files:
+#     txt_filename = os.path.splitext(os.path.basename(pdf_file))[0] + '.txt'
+#     txt_filepath = os.path.join(txt_folder_path, txt_filename)
+    
+#     # Extract entry data
+#     entry = entries_dict.get(os.path.basename(pdf_file))
+#     if entry is None:
+#         print(f"No entry found for {os.path.basename(pdf_file)}. Skipping this file.")
+#         continue
+
+#     authors = clean_author_names(entry['author'])
+#     journal = entry['journal']
+#     year = entry['year']
+#     title = remove_bibtex_formats(entry['title'])
+
+#     # preamble = f"This is an article by {authors} published in {journal} in {year} entitled {title}. \n \n"
+#     preamble = f"This is an article by {authors} published in {journal} in {year} entitled {title}"
+#     preamble = re.sub('\s+', ' ', preamble) + '\n\n'
+
+    
+#     pdf_to_text(pdf_file, txt_filepath, preamble)
+
+#     # Rename txt files based on the journal and year
+#     txt_files = glob.glob(os.path.join(txt_folder_path, "*.txt"))
+#     for txt_file in txt_files:
+#         old_name = os.path.basename(txt_file)
+#         filename, ext = os.path.splitext(old_name)
+#         year = re.findall(r"\d{4}", filename)[-1]
+#         journal = entry['journal']
+#         # Replace illegal characters with underscores
+#         journal_short = re.search('\((.*?)\)', journal)
+#         if journal_short is not None:
+#             journal_short = journal_short.group(1).replace(':', '').replace('/', '_').rstrip().replace(' ', '_')
+#         else:
+#             journal_short = journal.replace(':', '').replace('/', '_').rstrip().replace(' ', '_')
+#         new_name = f"{filename.rsplit('_', maxsplit=1)[0]}_{journal_short}{ext}"
+#         os.rename(txt_file, os.path.join(txt_folder_path, new_name))
+    
+    
+# print("Done converting PDFs to txt files.")
 
 # Convert each PDF to a txt file and save it in the 'txt' folder
 print("Converting PDFs to txt files...")
 pdf_files = glob.glob(os.path.join(pdf_folder_path, "*.pdf"))
 for pdf_file in pdf_files:
-    txt_filename = os.path.splitext(os.path.basename(pdf_file))[0] + '.txt'
-    txt_filepath = os.path.join(txt_folder_path, txt_filename)
-    
     # Extract entry data
     entry = entries_dict.get(os.path.basename(pdf_file))
     if entry is None:
@@ -87,34 +154,20 @@ for pdf_file in pdf_files:
         continue
 
     authors = clean_author_names(entry['author'])
-    journal = entry['journal']
+    journal = entry.get('journal', 'Unknown Journal')  # Update this line
     year = entry['year']
     title = remove_bibtex_formats(entry['title'])
 
-    # preamble = f"This is an article by {authors} published in {journal} in {year} entitled {title}. \n \n"
     preamble = f"This is an article by {authors} published in {journal} in {year} entitled {title}"
     preamble = re.sub('\s+', ' ', preamble) + '\n\n'
-
+    
+    # Generate filename based on your logic in bibtex_processing.py
+    txt_filename = generate_pattern(entry, mode=2) + '.txt'
+    txt_filepath = os.path.join(txt_folder_path, txt_filename)
     
     pdf_to_text(pdf_file, txt_filepath, preamble)
 
-    # Rename txt files based on the journal and year
-    txt_files = glob.glob(os.path.join(txt_folder_path, "*.txt"))
-    for txt_file in txt_files:
-        old_name = os.path.basename(txt_file)
-        filename, ext = os.path.splitext(old_name)
-        year = re.findall(r"\d{4}", filename)[-1]
-        journal = entry['journal']
-        # Replace illegal characters with underscores
-        journal_short = re.search('\((.*?)\)', journal)
-        if journal_short is not None:
-            journal_short = journal_short.group(1).replace(':', '').replace('/', '_').rstrip().replace(' ', '_')
-        else:
-            journal_short = journal.replace(':', '').replace('/', '_').rstrip().replace(' ', '_')
-        new_name = f"{filename.rsplit('_', maxsplit=1)[0]}_{journal_short}{ext}"
-        os.rename(txt_file, os.path.join(txt_folder_path, new_name))
-    
-    
 print("Done converting PDFs to txt files.")
+
 
 
